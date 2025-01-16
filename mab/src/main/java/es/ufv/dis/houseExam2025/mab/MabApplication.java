eda12c13aebe6d203d762f7f0f56160df4933159
Marcos Aparicio <marcosapa9@gmail.com>
2025-01-16 19:29:42 +0100

    Backend and Frontend setup.

---
+++ mab/src/main/java/es/ufv/dis/houseExam2025/mab/MabApplication.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.houseExam2025.mab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MabApplication.class, args);
+	}
+
+}
---
+++ mab/src/test/java/es/ufv/dis/houseExam2025/mab/MabApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.houseExam2025.mab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
